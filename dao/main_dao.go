c32eed430043ed07ed0861f6b58b7f5db6210c7b
yuriafujii <126153199+summerbee0504@users.noreply.github.com>
2023-11-11 12:52:27 +0900

    接続

--- dao/main_dao.go
+++ dao/main_dao.go
@@ -4,27 +4,20 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 )
 
 var Db *sql.DB
 
 func Init() {
 
-	//if err := godotenv.Load(".env_mysql"); err != nil {
-	//	log.Fatalf("fail: godotenv.Load, %v\n", err)
-	//}
-	//mysqlUser := os.Getenv("MYSQL_USER")
-	//mysqlPwd := os.Getenv("MYSQL_PWD")
-	//mysqlHost := os.Getenv("MYSQL_HOST")
-	//mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPwd := os.Getenv("MYSQL_PWD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
-	mysqlUser := "test_user"
-	mysqlPwd := "password"
-	mysqlDatabase := "hackathon_local"
-
-	//connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	//_db, err := sql.Open("mysql", connStr)
-	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlPwd, mysqlDatabase))
+	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	_db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
